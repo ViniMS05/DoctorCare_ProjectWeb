1b23b0acd42e433463b9cd3ba48944f02fb33f59
Vinicius <vmsilva1227@gmail.com>
2022-05-11 00:28:41 -0300

    Stage 4

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+
 function onScroll() {
+  showNavOnScroll()
+  showBackToTopOnScroll()
+};
+
+function showNavOnScroll() {
   var navigation = document.querySelector("#navigation");
 
   if (scrollY > 0) {
@@ -6,15 +15,31 @@ function onScroll() {
   } else {
     navigation.classList.remove("scroll");
   }
-}
+};
+
+function showBackToTopOnScroll () {
+  var backToTopButton = document.querySelector("#backToTopButton");
+
+  if (scrollY > 800) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+
+  if (scrollY > 4420) {
+    backToTopButton.classList.add("change");
+  } else {
+    backToTopButton.classList.remove("change");
+  }
+};
 
 function openMenu() {
   document.body.classList.add("menu-expanded");
-}
+};
 
 function closeMenu() {
   document.body.classList.remove("menu-expanded");
-}
+};
 
 ScrollReveal({
   origin: "top",
