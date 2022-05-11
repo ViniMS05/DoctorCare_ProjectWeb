4e2829535a7760a7c4b1c58a6f192228cd79363d
Vinicius <vmsilva1227@gmail.com>
2022-05-10 21:22:15 -0300

    Stage 3

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-function onScroll () {
-    var navigation = document.querySelector('#navigation') 
+function onScroll() {
+  var navigation = document.querySelector("#navigation");
 
-    if (scrollY > 0) {
-        navigation.classList.add('scroll')
-    } else {
-        navigation.classList.remove('scroll')
-    }
+  if (scrollY > 0) {
+    navigation.classList.add("scroll");
+  } else {
+    navigation.classList.remove("scroll");
+  }
 }
 
-function openMenu () {
-    document.body.classList.add('menu-expanded')
+function openMenu() {
+  document.body.classList.add("menu-expanded");
 }
 
-function closeMenu () {
-    document.body.classList.remove('menu-expanded')
+function closeMenu() {
+  document.body.classList.remove("menu-expanded");
 }
+
+ScrollReveal({
+  origin: "top",
+  distance: "50px",
+  durstion: 700,
+}).reveal(
+  "#home, #home img, #home .datas, #services, #services header, #services .card, #about header, #about .content"
+);
